eeb5bf0a14fac96dcf95d056b6084248523a61a6
Stenis76 <stenqvist.dick@gmail.com>
2020-04-21 09:43:09 +0200

    upp and running
    Co-authored-by: adam.kjall@medieinstitutet.se <adam.kjall@medieinstitutet.se>

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+import io from "socket.io-client";
+
+import "./App.css";
 
 function App() {
+  // component did mount
+  useEffect(() => {
+    const socket = io("http://localhost:8080");
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
+
+const PORT = 8080;
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket " + socket.id);
+});
+
+http.listen(PORT, () => console.log("Listening to requests on port: ", PORT));
