ca55ad1b4fcba6e9ee3fb7e0384ed5c69de369aa
Stenis76 <stenqvist.dick@gmail.com>
2020-04-27 21:35:44 +0200

    lösenord funkar

--- client/src/components/room-sidebar/index.jsx
+++ client/src/components/room-sidebar/index.jsx
@@ -4,8 +4,29 @@ import RoomList from "../room-list";
 import InputBox from "../input-box";
 
 import "./styles.scss";
+import { useState } from "react";
 
 const RoomSidebar = ({ rooms, currentRoom, joinRoom }) => {
+
+  const [checked, setChecked] = useState(false)
+  const handleClick = () => setChecked(!checked)
+
+  const [password, setPassword] = useState("");
+
+  const roomToCreate = (room) => {
+    console.log(room);
+    if (checked == true) {
+      joinRoom(room, password)
+      console.log("password is " + password);
+
+    }
+    else {
+
+      joinRoom(room, null)
+      console.log("no password");
+
+    }
+  }
   return (
     <div className="room-sidebar">
       <header className="sidebar-header">
@@ -14,11 +35,16 @@ const RoomSidebar = ({ rooms, currentRoom, joinRoom }) => {
       <main className="sidebar-content">
         <RoomList rooms={rooms} joinRoom={joinRoom} currentRoom={currentRoom} />
         <InputBox
-          callback={joinRoom}
+          callback={roomToCreate}
           title={"create"}
           type="text"
           placeholder="Write name of new room"
         />
+        <div>
+          <label htmlFor="activate-password">Password on room</label>
+          <input onClick={handleClick} checked={checked} type="checkbox" />
+          <input type="text" placeholder="Choose a password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
       </main>
     </div>
   );
